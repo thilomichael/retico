4cb11b37899b64724348045e5e7fe9fa7212faab
Thilo Michael <thilo.michael@tu-berlin.de>
2018-02-19 15:56:52 +0100

    Added documentation for headless execution.

--- retico/headless.py
+++ retico/headless.py
@@ -1,25 +1,57 @@
+"""A Module that allows for saving and loading networks from and to file."""
+
 import sys
 import pickle
 
-def load_and_execute(filename):
-    """Runs a retico network by its given filename."""
+
+def load(filename):
+    """Loads a network from file and returns a list of modules in that network.
+
+    The connections between the module have been set according to the file.
+
+    Args:
+        filename (str): The path to the .rtc file containing a network.
+
+    Returns:
+        (list): A list of Modules that are connected and ready to be run.
+    """
     mc_list = pickle.load(open(filename, "rb"))
     module_dict = {}
+    module_list = []
     for m in mc_list[0]:
         mod = m["retico_class"](**m["retico_args"])
         module_dict[m["id"]] = mod
+        module_list.append(mod)
     for ida, idb in mc_list[1]:
         module_dict[idb].subscribe(module_dict[ida])
 
-    for _, module in module_dict.items():
-        module.run()
+    return module_list
+
+
+def load_and_execute(filename):
+    """Loads a network from file and runs it.
+
+    The network is loaded via the load-Method. Before running the network, it is
+    setup.
+
+    Args:
+        filename (str): The path to the .rtc file containing a network.
+    """
+    module_list = load(filename)
+
+    for module in module_list:
+        module.setup()
+
+    for module in module_list:
+        module.run(run_setup=False)
 
     input()
 
-    for _, module in module_dict.items():
+    for module in module_list:
         module.stop()
 
-def discover_all_modules(module):
+
+def _discover_modules(module):
     discovered_lb = []
     discovered_rbs = []
     lb = module.left_buffer()
@@ -30,7 +62,24 @@ def discover_all_modules(module):
             discovered_rbs.append(rb.consumer)
     return set(discovered_lb), set(discovered_rbs)
 
+
 def save(module, filename):
+    """Saves a network to file given a module or a list of modules.
+
+    The network is automatically detected by traversing all left and right
+    buffers of the modules given. If the argument module is only a single
+    module, the network that is being saved consists only of the module
+    reachable from this module. If a network should be saved that is splitted
+    into multiple parts, at least one module of each split has to be included
+    into the module-list.
+
+    Args:
+        module (AbstractModule or list): A module of the network or a list of
+            multiple modules of the network.
+        filename (str): The path to where the network should be stored. This
+            excludes the file-ending .rtc that will be automatically added by
+            this function.
+    """
     if not isinstance(module, list):
         module = [module]
     undiscovered = set(module)
@@ -40,7 +89,7 @@ def save(module, filename):
     while undiscovered:
         current_module = undiscovered.pop()
         discovered.append(current_module)
-        lbs, rbs = discover_all_modules(current_module)
+        lbs, rbs = _discover_modules(current_module)
         for mod in lbs:
             if mod not in discovered:
                 undiscovered.add(mod)
@@ -58,6 +107,7 @@ def save(module, filename):
         for buf in current_module.right_buffers():
             c_list.append((id(buf.consumer), id(buf.provider)))
     pickle.dump([m_list, c_list], open("%s.rtc" % filename, "wb"))
+
 
 if __name__ == '__main__':
     load_and_execute(sys.argv[1])
