dc25fa44808a52ae34ade0f30c78637ac5ccfd36
Thilo Michael <thilo.michael@tu-berlin.de>
2019-07-14 21:47:28 +0200

    Changed dialogue act transcriber to match annotation format.

--- retico/core/dialogue/io.py
+++ retico/core/dialogue/io.py
@@ -1,5 +1,6 @@
 
 import json
+import time
 
 from retico.core.abstract import AbstractConsumingModule, AbstractTriggerModule
 from retico.core.dialogue.common import DispatchableActIU, DialogueActIU
@@ -28,6 +29,9 @@ class DialogueActRecorderModule(AbstractConsumingModule):
     def setup(self):
         self.txt_file = open(self.filename, "w")
 
+    def prepare_run(self):
+        self.start_time = time.time()
+
     def shutdown(self):
         if self.txt_file:
             self.txt_file.close()
@@ -35,13 +39,20 @@ class DialogueActRecorderModule(AbstractConsumingModule):
 
     def process_iu(self, input_iu):
         if self.txt_file:
-            self.txt_file.write(str(input_iu.creator))
+            self.txt_file.write("dialogue_act")
             self.txt_file.write(self.separator)
-            self.txt_file.write(str(input_iu.created_at))
+            self.txt_file.write(str(input_iu.creator).split(" ")[-1])
             self.txt_file.write(self.separator)
-            self.txt_file.write(input_iu.act)
+            if input_iu.created_at < self.start_time:
+                self.start_time = input_iu.created_at
+            self.txt_file.write(str(int((input_iu.created_at-self.start_time)*1000)))
             self.txt_file.write(self.separator)
-            self.txt_file.write(json.dumps(input_iu.concepts))
+            self.txt_file.write("-1")
+            self.txt_file.write(self.separator)
+            if input_iu.concepts.keys():
+                self.txt_file.write(input_iu.act+":"+",".join(input_iu.concepts.keys()))
+            else:
+                self.txt_file.write(input_iu.act)
             if isinstance(input_iu, DispatchableActIU):
                 self.txt_file.write(self.separator)
                 self.txt_file.write(str(input_iu.dispatch))
